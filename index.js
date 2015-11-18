c75b102b670b55f2d074c20027658e95c9ea70ca
gshenj <gshenj@gmail.com>
2015-11-18 23:19:13 +0800

    add sum

--- index.js
+++ index.js
@@ -23,9 +23,9 @@ var session = {};
 // initialization and is ready to create browser windows.
 app.on('ready', function() {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 900,
-   height: 700
-  //,'auto-hide-menu-bar': true
+  mainWindow = new BrowserWindow({width: 1000,
+   height: 600
+  ,'auto-hide-menu-bar': true
   });
 
   // and load the index.html of the app.
