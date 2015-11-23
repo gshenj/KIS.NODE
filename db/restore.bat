2189521f3fc8beb7c131e44af02aeead0c699311
gshenj <gshenj@gmail.com>
2015-11-23 16:01:59 +0800

    add db back

--- db/restore.bat
+++ db/restore.bat
@@ -1,2 +1,6 @@
 "C:\Program Files\PostgreSQL\9.4\bin\psql" -h localhost -U postgres kisweb < E:\Programming\GitHub\KIS.NODE\db\kisweb.1.bak
  pause
+
+
+
+"C:\Program Files\PostgreSQL\9.4\bin\pg_dump" -h localhost -U postgres kisweb >  E:\Programming\GitHub\KIS.NODE\db\kisweb.bak.20151123
