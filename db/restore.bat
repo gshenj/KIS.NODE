cc5cb7bfe31e6a322cd6f487a2dca3fabb1dc9c2
gshenj <gshenj@gmail.com>
2016-04-15 10:00:00 +0800

    New changelist

--- db/restore.bat
+++ db/restore.bat
@@ -1,7 +1,8 @@
 C:\Program Files\PostgreSQL\9.4\bin\createdb" newdatabase
-"C:\Program Files\PostgreSQL\9.4\bin\psql" -h localhost -U postgres kisweb < E:\Programming\GitHub\KIS.NODE\db\kisweb.bak.20151123
+"D:\ProgramFiles\PostgreSQL\9.4\bin\psql.exe" -h localhost -U postgres kisweb < D:\Programming\Github\KIS.NODE\db\kisweb.bak.20151123
  pause
 
-
+D:\ProgramFiles\PostgreSQL\9.4\bin
 
 "C:\Program Files\PostgreSQL\9.4\bin\pg_dump" -h localhost -U postgres kisweb >  E:\Programming\GitHub\KIS.NODE\db\kisweb.bak.20151123
+
